5e4c3888bd21d512117ecf72ce3d582739561821
Aditya Savara <39929571+AdityaSavara@users.noreply.github.com>
2020-10-31 17:20:41 -0400

    Add files via upload

---
+++ kmos/snapshots_globals.py
@@ -0,0 +1,24 @@
+#this file is normally used by "import snapshots_globals as sg"
+from kmos.run import KMC_Model
+from kmc_settings import *
+simulation_name = model_name
+start = 0.0
+model = KMC_Model()
+atoms = model.get_atoms(geometry=False)
+occ_header_string = model.get_occupation_header()
+occ_header_array = occ_header_string.split()
+TOF_header_string = model.get_tof_header()
+TOF_header_array= TOF_header_string.split()
+data_file_name = simulation_name + str('TOFs_and_Coverages.csv')
+parameters_of_interest = ['T']
+steps_so_far = 0
+snapshots_so_far = 0
+write_output = 'True'
+TOF_data_list = []
+TOF_integ_list = []
+occ_data_list = []
+last_snapshot_outputs = []
+snapshot_output_headers = []
+sp_steps = 0.0
+sp_steps_initial = 0.0
+#to get atoms.kmc_time access it by from sg.atoms.kmc_time
