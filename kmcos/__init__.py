4447ad98bbae6dd3f6f6c4816032f771e4b9f4cb
AdityaSavara <39929571+AdityaSavara@users.noreply.github.com>
2020-12-19 14:48:26 -0500

    Update __init__.py

--- kmcos/__init__.py
+++ kmcos/__init__.py
@@ -438,37 +438,39 @@ def evaluate_rate_expression(rate_expr, parameters={}):
 
 #kmcos can be invoked directly from the command line in one of the following ways::
 #    kmcos [help] (all|benchmark|build|edit|export|help|import|rebuild|run|settings-export|shell|version|view|xml) [options]
-#defining small wrapper functions to call cli if person types kmcos.export("MyFirstModel.xml") etc. Probably should have made an "@", but that's okay.
+#defining small wrapper functions to call cli if person types kmcos.export("MyFirstModel.xml") etc. Probably should have made an "@" decorator, but that's okay.
 def build(argumentsString):
-    import cli
-    cli.main('build' + argumentsString)
+    import kmcos.cli as cli
+    cli.main('build' +' ' + argumentsString)
 def edit(argumentsString):
-    import cli
-    cli.main('edit' + argumentsString)
+    import kmcos.cli as cli
+    cli.main('edit'+' ' + argumentsString)
 def export(argumentsString):
-    import cli
-    cli.main('export' + argumentsString)
+    import kmcos.cli as cli
+    cli.main('export'+' ' + argumentsString)
 def help(argumentsString):
-    import cli
-    cli.main('help' + argumentsString)
+    import kmcos.cli as cli
+    cli.main('help' +' '+ argumentsString)
 def rebuild(argumentsString):
-    import cli
-    cli.main('rebuild' + argumentsString)
+    import kmcos.cli as cli
+    cli.main('rebuild'+' ' + argumentsString)
 def run(argumentsString):
-    import cli
-    cli.main('run' + argumentsString)
+    import kmcos.cli as cli
+    cli.main('run'+' ' + argumentsString)
 def settings_export(argumentsString):
-    import cli
-    cli.main('settings-export' + argumentsString)
+    import kmcos.cli as cli
+    cli.main('settings-export'+' ' + argumentsString)
 def shell(argumentsString):
-    import cli
-    cli.main('shell' + argumentsString)    
+    import kmcos.cli as cli
+    cli.main('shell'+' ' + argumentsString)    
 def version(argumentsString):
-    import cli
-    cli.main('version' + argumentsString)    
+    import kmcos.cli as cli
+    cli.main('version'+' ' + argumentsString)    
 def view(argumentsString):
-    import cli
-    cli.main('view' + argumentsString)    
+    import kmcos.cli as cli
+    cli.main('view'+' ' + argumentsString)    
 def xml(argumentsString):
-    import cli
-    cli.main('xml' + argumentsString)    
+    import kmcos.cli as cli
+    cli.main('xml'+' ' + argumentsString)    
+
+from kmcos.io import clear_model
