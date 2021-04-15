c7f58d9a5e176262adad8ade46acb2881a0e7a33
AdityaSavara <39929571+AdityaSavara@users.noreply.github.com>
2021-04-14 21:10:00 -0400

    Version update

--- kmcos/__init__.py
+++ kmcos/__init__.py
@@ -51,7 +51,7 @@ from __future__ import print_function
 #import kmcos.types
 #import kmcos.io
 
-__version__ = "0.0.40"
+__version__ = "0.0.50"
 VERSION = __version__
 
 def evaluate_param_expression(param, parameters={}):
