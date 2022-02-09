8d1273dd966cb94e8c6b5e1fdd5b9b96f85f2607
Meelod Waheed <meelod.waheed360@gmail.com>
2022-02-09 12:05:00 -0500

    create_kmc_model

--- tests/test_cli_generation.py
+++ tests/test_cli_generation.py
@@ -15,45 +15,45 @@ def generate_model():
         Site, \
         Species
 
-    project = Project()
+    kmc_model = kmcos.create_kmc_model()
 
     # set meta information
     model_name = 'test_cli_generated_model'
-    project.meta.author = 'Max J. Hoffmann'
-    project.meta.email = 'mjhoffmann@gmail.com'
-    project.meta.model_dimension = '2'
-    project.meta.debug = 0
-    project.meta.model_name = model_name
+    kmc_model.meta.author = 'Max J. Hoffmann'
+    kmc_model.meta.email = 'mjhoffmann@gmail.com'
+    kmc_model.meta.model_dimension = '2'
+    kmc_model.meta.debug = 0
+    kmc_model.meta.model_name = model_name
 
     # add layer
-    project.add_layer(Layer(name='default', sites=[
+    kmc_model.add_layer(Layer(name='default', sites=[
         Site(name='cus', pos='0 0.5 0.5')]))
 
-    project.layer_list.default_layer = 'default'
+    kmc_model.layer_list.default_layer = 'default'
 
     # add species
-    project.add_species(Species(name='oxygen', color='#ff0000'))
-    project.add_species(Species(name='CO', color='#000000'))
-    project.add_species(Species(name='empty', color='#ffffff'))
-    project.species_list.default_species = 'empty'
+    kmc_model.add_species(Species(name='oxygen', color='#ff0000'))
+    kmc_model.add_species(Species(name='CO', color='#000000'))
+    kmc_model.add_species(Species(name='empty', color='#ffffff'))
+    kmc_model.species_list.default_species = 'empty'
 
     # add parameters
-    project.add_parameter(Parameter(name='p_CO', value=0.2, scale='log'))
-    project.add_parameter(Parameter(name='T', value=500, adjustable=True))
-    project.add_parameter(Parameter(name='p_O2', value=1.0, adjustable=True))
+    kmc_model.add_parameter(Parameter(name='p_CO', value=0.2, scale='log'))
+    kmc_model.add_parameter(Parameter(name='T', value=500, adjustable=True))
+    kmc_model.add_parameter(Parameter(name='p_O2', value=1.0, adjustable=True))
 
     # add processes
     cus = Coord(name='cus', layer='default')
     p = Process(name='CO_adsorption', rate_constant='1000.')
     p.add_condition(ConditionAction(species='empty', coord=cus))
     p.add_action(ConditionAction(species='CO', coord=cus))
-    project.add_process(p)
+    kmc_model.add_process(p)
 
     p = Process(name='CO_desorption', rate_constant='1000.')
     p.add_condition(ConditionAction(species='CO', coord=cus))
     p.add_action(ConditionAction(species='empty', coord=cus))
-    project.add_process(p)
-    return project
+    kmc_model.add_process(p)
+    return kmc_model
 
 
 def test_model_generation_and_export():
