003405642a5b96d5ccf0bf9364e42b4c46d3c76e
Meelod Waheed <meelod.waheed360@gmail.com>
2022-02-24 10:46:36 -0500

    Puts number of steps in pickle filename

--- examples/MyFirstDiffusion_local_smart/runfile_atoms_export.py
+++ examples/MyFirstDiffusion_local_smart/runfile_atoms_export.py
@@ -4,4 +4,4 @@ from kmcos.run import KMC_Model
 model = KMC_Model()
 NSTEPS = 1e6
 model.do_steps(NSTEPS)
-model.pickle_export_atoms("MyFirstDiffusion_atoms.ui")
+model.pickle_export_atoms("MyFirstDiffusion_atoms_" + str(NSTEPS) + ".pkl")
