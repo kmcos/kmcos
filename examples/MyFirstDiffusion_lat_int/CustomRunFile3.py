d290b2a4ecbdc324c6b55f127b68d1962d7a3f1f
Meelod Waheed <meelod.waheed360@gmail.com>
2022-03-14 09:58:07 -0400

    Examples updated to use snapshots

--- examples/MyFirstDiffusion_lat_int/CustomRunFile3.py
+++ examples/MyFirstDiffusion_lat_int/CustomRunFile3.py
@@ -1,17 +1,21 @@
 '''This file is made to run with MyFirstDiffusion. This file does steps 1 by 1 and records the site occupations for each step at each site. '''
-import os
-import math 
-import pdb
-from kmcos.run import KMC_Model
 from kmc_settings import *
-model = KMC_Model()
-atoms = model.get_atoms(geometry=False)
+from kmcos.snapshots import *
+import kmcos.snapshots_globals as sg
+import os
+
+sg.parameters_of_interest = None #['T','R'] #<-- put the parameters you want exported with each snapshot here. Can also put activation energy etc.
+
+#The kmcos Model is initialized in create_headers
+create_headers()
+
+atoms = sg.model.get_atoms(geometry=False)
 
 # represents the full directory path name that the kmcos directory is in with directory name last, have to add the _ or we cannot retrieve the last character
 dirpath_name = os.getcwd() + '_'
 
 
-simulation_size = model.size[0] # model.size gives you the size of the arrays which would be [3,3] for a model
+simulation_size = sg.model.size[0] # model.size gives you the size of the arrays which would be [3,3] for a model
 totalsites = simulation_size*simulation_size#with a sim_size of 3. 
 
 spt = 20 # total number of steps
@@ -37,8 +41,8 @@ with open(filename1, 'a') as f1:
 		f1.write('%s;'%(atoms.occupation[0])[1])	
 		f1.write('%s\n'%(atoms.occupation[0])[2])
 
-		model.do_steps(sps)
+		sg.model.do_steps(sps)
 		Step += sps
-		atoms = model.get_atoms(geometry=False) # Have to recall it everytime or it will not refresh atoms
+		atoms = sg.model.get_atoms(geometry=False) # Have to recall it everytime or it will not refresh atoms
         
 print("Finished with Custom Runfile!")
--- examples/MyFirstDiffusion_local_smart/runfile_atoms_import.py
+++ examples/MyFirstDiffusion_local_smart/runfile_atoms_import.py
@@ -1,7 +1,10 @@
 from ase.io import write
 import pickle
 
-filehandler = open('MyFirstDiffusion_atoms_1000000.0.pkl', 'rb')
+#filehandler opens the pickle file from the runfile_atoms_export.py and unpickles it
+filehandler = open('MyFirstDiffusion_atoms_10.pkl', 'rb')
 loaded_atoms_object = pickle.load(filehandler)
 filehandler.close()
-write('MyFirstDiffusion_atoms_1000000.0.png', loaded_atoms_object)
+
+#write creates a .png image of the model's atomic view
+write('MyFirstDiffusion_atoms_10.png', loaded_atoms_object)
